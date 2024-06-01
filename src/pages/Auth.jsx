f1fea52cd31cc1127bda59ea578f71019e3bcc73
Emmanuel Balogun <emmanx25@gmail.com>
2024-06-01 23:43:08 +0100

    setup auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { auth, provider } from "../config/firebase-config"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from 'react-router-dom'
 
 const Auth = () => {
+  const navigate = useNavigate()
+
+  const signInWithGoogle = async () => {
+    const results = await signInWithPopup(auth, provider)
+    console.log(results)
+
+    const authInfo = {
+      displayName: results.user.displayName,
+      email: results.user.email,
+      photoURL: results.user.photoURL,
+      uid: results.user.uid,
+      isAuth: true
+    }
+    localStorage.setItem("authInfo", JSON.stringify(authInfo))
+    navigate("/expense_tracker")
+  }
+
   return (
-    <div>Auth</div>
+    <div className="">
+      <Button className="" onClick={signInWithGoogle}>Sign in with Google</Button>
+    </div>
   )
 }
 
