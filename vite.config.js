587e3cdfbb4192bc4817ee04c321f24614c3bfc3
Emmanuel Balogun <emmanx25@gmail.com>
2024-06-12 03:21:27 +0100

    Update npm dependencies, add vite-plugin-pwa for progressive web app functionality

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,43 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "Xpense",
+        short_name: "Xpense",
+        description:
+          "Xpense is an expense tracker web application that allows users to manage there expenses digitaly",
+        theme_color: "#09090b",
+        background_color: "#09090b",
+        icons: [
+          {
+            src: "/public/img.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   define: {
-    'process.env': process.env
-  }
-})
+    "process.env": process.env,
+  },
+});
